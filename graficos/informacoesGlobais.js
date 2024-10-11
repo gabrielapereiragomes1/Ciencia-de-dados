1f4286c9ba2ee2d1b308bb2ef012191ae835780a
gabrielapereiragomes1 <105368697+gabrielapereiragomes1@users.noreply.github.com>
2024-10-11 08:04:35 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,8 @@
 const url = 'https://raw.github.content.com/guilhermeonrails/api/main/dados-globai.json'
+
 async function vizualizarInformacoesGlobais() {
     const res = await fetch (url);
-    const dados = res. json()
+    const dados = res.json()
     console.log(dados);
 }
+vizualizarInformacoesGlobais()
