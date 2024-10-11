a9dd39bbb5fa88b54d036be186e8bbf2df8e00ae
gabrielapereiragomes1 <105368697+gabrielapereiragomes1@users.noreply.github.com>
2024-10-11 08:32:45 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,5 +5,7 @@ async function vizualizarInformacoesGlobais() {
     const dados = res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que estão aproximadamente ${dados.total_pessoa_conectadas} estão conectada em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
 }
 vizualizarInformacoesGlobais()
